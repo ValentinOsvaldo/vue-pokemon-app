62c6abee46729971c3f4f6a8ed428739d4408e2e
Osvaldo Valentin <69062379+ValentinOsvaldo@users.noreply.github.com>
2022-09-30 17:50:32 -0500

    ui

--- src/components/PokemonOptions.vue
+++ src/components/PokemonOptions.vue
@@ -1,16 +1,14 @@
 <template>
   <div class="options-container">
-    <ul>
-      <li
+    <button
       as="button"
-        v-for="pokemon in pokemons"
-        :key="pokemon.id"
-      >
-        <button
-        :disabled="showOptions ? false : true"
-        @click="$emit('selection', pokemon.id)">{{pokemon.name}}</button>
-      </li>
-    </ul>
+      v-for="pokemon in pokemons"
+      :key="pokemon.id"
+      :disabled="showOptions ? false : true"
+      @click="$emit('selection', pokemon.id)"
+    >
+      {{ pokemon.name }}
+    </button>
   </div>
 </template>
 
@@ -29,43 +27,40 @@ export default {
   },
   created() {
     // props are exposed on `this`
-    console.log('Component showOptions :'+this.showOptions)
+    console.log('Component showOptions :' + this.showOptions);
   },
-  watch :{
+  watch: {
     showOptions() {
-      console.log('Component showOptions: '+this.showOptions)
-    }
-  }
+      console.log('Component showOptions: ' + this.showOptions);
+    },
+  },
 };
 </script>
 
 <style scoped>
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
+button {
   background-color: white;
   border-radius: 5px;
   border: 1px solid rgba(0, 0, 0, 0.2);
   cursor: pointer;
+  font-size: 1rem;
   margin-bottom: 10px;
   padding: 0.5rem 0.25rem;
   width: 250px;
 }
 
-li:hover {
+button:hover {
   background-color: rgba(0, 0, 0, 0.05);
 }
-li:disabled {
-  pointer-events:none; 
-  opacity:0.6;      
+button:disabled {
+  pointer-events: none;
+  opacity: 0.6;
 }
-
 .options-container {
   display: flex;
   justify-content: center;
   align-items: center;
+  flex-flow: column nowrap;
+  margin-top: 1rem;
 }
 </style>
--- src/pages/PokemonPage.vue
+++ src/pages/PokemonPage.vue
@@ -6,21 +6,25 @@
     <!-- Imagen -->
     <PokemonPicture :pokemonId="pokemon.id" :showPokemon="showPokemon" />
     <!-- Opciones -->
-    <PokemonOptions :pokemons="pokemonArr" :showOptions="!showAnswer" @selection="checkAnswer" />
+    <PokemonOptions
+      :pokemons="pokemonArr"
+      :showOptions="!showAnswer"
+      @selection="checkAnswer"
+    />
     <template v-if="showAnswer">
       <h2 class="fade-in">{{ message }}</h2>
     </template>
-    <h1>Puntuaci&oacute;n: {{score}} puntos</h1>
-    <h3>Record: {{highScore}} puntos</h3>
+    <h1>Puntuaci&oacute;n: {{ score }} puntos</h1>
+    <h3>Record: {{ highScore }} puntos</h3>
     <button @click="newGame">Reiniciar juego</button>
   </div>
 </template>
 
 <script>
-import PokemonOptions from "@/components/PokemonOptions.vue";
-import PokemonPicture from "@/components/PokemonPicture.vue";
+import PokemonOptions from '@/components/PokemonOptions.vue';
+import PokemonPicture from '@/components/PokemonPicture.vue';
 
-import getPokemonOptions from "@/helpers/getPokemonsOptions";
+import getPokemonOptions from '@/helpers/getPokemonsOptions';
 
 export default {
   components: { PokemonPicture, PokemonOptions },
@@ -30,10 +34,10 @@ export default {
       pokemon: null,
       showPokemon: false,
       showAnswer: false,
-      message: "",
+      message: '',
       score: 0, // Added a score to data so we can play more thank once. rPerez
       highScore: 0, // Added so we can show the highScore, used localStorage to save it
-      turn: 1 // Added turn so we can determine when the game is over, as opt-in the user can restart the game at anytime 
+      turn: 1, // Added turn so we can determine when the game is over, as opt-in the user can restart the game at anytime
     };
   },
   methods: {
@@ -48,61 +52,77 @@ export default {
       this.showPokemon = true;
       this.showAnswer = true;
       // we set the turn to turn+1
-      this.turn = Number.parseInt(this.turn) + 1
+      this.turn = Number.parseInt(this.turn) + 1;
       if (selectedId === this.pokemon.id) {
         this.message = `Correcto, ${this.pokemon.name}`;
         // Once we checked its an correct answer we add 15 to the current score
-        this.score = Number.parseInt(this.score) + 15
-        localStorage.setItem('score',this.score)
+        this.score = Number.parseInt(this.score) + 15;
+        localStorage.setItem('score', this.score);
       } else {
         this.message = `Oops, es ${this.pokemon.name}`;
       }
-      setTimeout(this.nextPokemon, 2000)
+      setTimeout(this.nextPokemon, 2000);
     },
     nextPokemon() {
-
       this.showPokemon = false;
       this.showAnswer = false;
-      if(this.turn==10){
-        this.turn = 1
-        alert('Juego terminado')
-        let oldHighScore = localStorage.getItem('highScore')
-        if(this.score>oldHighScore){
-          alert(`Juego terminado \n Haz alcanzado un nuevo record!`)
-          localStorage.setItem('highScore', this.score)
-          this.highScore = this.score
+      if (this.turn == 10) {
+        this.turn = 1;
+        alert('Juego terminado');
+        let oldHighScore = localStorage.getItem('highScore');
+        if (this.score > oldHighScore) {
+          alert(`Juego terminado \n Haz alcanzado un nuevo record!`);
+          localStorage.setItem('highScore', this.score);
+          this.highScore = this.score;
         } else {
-          alert(`Juego terminado, no haz logrado batir tu record, sigue intentando!`)
+          alert(
+            `Juego terminado, no haz logrado batir tu record, sigue intentando!`
+          );
         }
-        this.score = 0
-        localStorage.removeItem('score')
+        this.score = 0;
+        localStorage.removeItem('score');
       }
       this.pokemon = null;
       this.pokemonArr = [];
       this.mixPokemonArray();
     },
     newGame() {
-      this.score = 0
-      localStorage.removeItem('score')
-      this.nextPokemon()
-    }
+      this.score = 0;
+      localStorage.removeItem('score');
+      this.nextPokemon();
+    },
   },
   mounted() {
     this.mixPokemonArray();
-    if(localStorage.getItem('highScore')!==null){
-      this.highScore = localStorage.getItem('highScore')
+    if (localStorage.getItem('highScore') !== null) {
+      this.highScore = localStorage.getItem('highScore');
     }
   },
-  watch :{
+  watch: {
     showAnswer() {
-      console.log('Answer choosen')
-      console.log(this.showAnswer)
+      console.log('Answer choosen');
+      console.log(this.showAnswer);
     },
-    pokemonArr() {
-      
-    }
-  }
+    pokemonArr() {},
+  },
 };
 </script>
 
-<style></style>
+<style scoped>
+button {
+  background-color: cornflowerblue;
+  border: thin solid cornflowerblue;
+  border-radius: 0.25rem;
+  color: #fff;
+  cursor: pointer;
+  font-size: 1rem;
+  font-weight: 500;
+  padding: 0.5rem 1rem;
+  transition: all 0.3s ease;
+}
+
+button:hover {
+  background-color: rgb(100, 149, 237, 0.10);
+  color: #000;
+}
+</style>
